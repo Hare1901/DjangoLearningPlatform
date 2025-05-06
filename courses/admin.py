3c7f081ee9ed606fde724a909955bca71b4df3c8
Hare1901 <hare1901@yandex.ru>
2025-05-06 06:19:10 +0300

    FEAT: Админ панель для классов Course, Subject, Module

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, Subject, Module
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.TabularInline):
+    model = Module
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'subjects', 'created']
+    list_filter = ['subjects', 'created']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
+
