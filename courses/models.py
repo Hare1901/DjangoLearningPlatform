81fd06aa43229d919e9ce50a9a81566cd7ce3b1d
Hare1901 <hare1901@yandex.ru>
2025-05-11 06:45:56 +0300

    FEAT: Модель для мультиконтента в модулях

--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
+from django.utils import timezone
+
 from django.contrib.auth.models import User
 
-class Subject(models.Model):
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
+class Subject(models.Model):
+    """
+    Модель для определения темы
+    """
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
 
@@ -14,7 +21,9 @@ class Subject(models.Model):
         return self.title
 
 class Course(models.Model):
-
+    """
+    Модель для определения курса
+    """
     owner = models.ForeignKey(User, related_name='courses_created', on_delete=models.CASCADE)
     subjects = models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -29,9 +38,27 @@ class Course(models.Model):
         return self.title
 
 class Module(models.Model):
+    """
+    Модель для определения модулей
+    """
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    """
+    Модель для мультиконтента модулей
+    """
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    created = models.DateTimeField(default=timezone.now)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order','created']
