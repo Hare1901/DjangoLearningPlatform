4763a191ac7cee14c19bd1f56f8cffb5efe7dc22
Hare1901 <hare1901@yandex.ru>
2025-05-13 08:10:53 +0300

    FEAT: Модель для 4 основных видов контента, через общий абстрактный класс

--- courses/models.py
+++ courses/models.py
@@ -54,7 +54,10 @@ class Content(models.Model):
     Модель для мультиконтента модулей
     """
     module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, limit_choices_to={'model__in': ('text',
+                                                                                  'video',
+                                                                                  'image',
+                                                                                  'file')}, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
     created = models.DateTimeField(default=timezone.now)
@@ -62,3 +65,31 @@ class Content(models.Model):
 
     class Meta:
         ordering = ['order','created']
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related')
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
